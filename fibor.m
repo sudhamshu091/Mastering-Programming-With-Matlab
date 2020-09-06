1917684fce48734dc105a2db0b3bdcefef99ad0b
Sudhamshu B N <66492363+sudhamshu091@users.noreply.github.com>
2020-09-06 13:28:20 +0530

    Update fibor.m

--- fibor.m
+++ fibor.m
@@ -1,3 +1,9 @@
+%Write a recursive function called fibor that computes the first n elements of the Fibonacci series where n is sole input argument. You are not allowed to use loops.
+%F(n) ={ 1             if n<=2
+%        F(n-1)+F(n-2) otherwise }
+%Here are the first few elements: 1 1 2 3 5 8 13 ...
+% Don't use fibor() call more than once for efficient functioning.
+        
 function f = fibor(n)
     if n==1
         f=[1];
