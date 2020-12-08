4af13e75537db22c55941905b48048b1d81ba00b
Sudhamshu B N <66492363+sudhamshu091@users.noreply.github.com>
2020-12-08 23:26:36 +0530

    Create name_value_pairs_official_solution.m

---
+++ name_value_pairs_official_solution.m
@@ -0,0 +1,18 @@
+function store = name_value_pairs(varargin)
+    % Initialize an empty cell array
+    store = {};
+    % If the input does not come in pairs (remainder = 1), return
+    if rem(length(varargin),2)
+        return;
+    end
+    
+    for ii = 1:floor(length(varargin)/2)
+        % The Name part must be a char. If not, return an empty cell
+        if ~ischar(varargin{2*ii-1})
+            store = {};
+            return;
+        end
+        store{ii,1} = varargin{2*ii-1};
+        store{ii,2} = varargin{2*ii};
+    end
+end
