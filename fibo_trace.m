4e576c0112a8e909c526bcbbf4d912b1a89eb5af
Sudhamshu B N <66492363+sudhamshu091@users.noreply.github.com>
2020-12-09 23:28:25 +0530

    Create fibo_trace.m

---
+++ fibo_trace.m
@@ -0,0 +1,13 @@
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+function [f, trace] = fibo_trace(n, trace)
+    trace = [trace,n]; 
+    if n<=2
+        f = 1;
+    else
+        [f1, trace] = fibo_trace(n-2, trace); 
+        [f2, trace] =  fibo_trace(n-1, trace);
+        f = f1+f2;  
+    end 
+end
