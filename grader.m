1edc4781c154bc79a49a69ab09a43ec223bc42f5
Sudhamshu B N <66492363+sudhamshu091@users.noreply.github.com>
2020-12-09 13:32:32 +0530

    Update grader.m

--- grader.m
+++ grader.m
@@ -1,9 +1,9 @@
-Here is you chance to write an autograder! Specifically, write a function called grader that tests two functions (one is supposed to be the student's solution and the other the
-reference solution provided by the instructor) by calling them repeatedly with various input arguments and comparing the results. For simplicity, we assume that both functions take
-exactly one input argument. The inputs to the grader function handles followed by a variable number of additional input arguments. The function must call the two
-functions with each of the supplied input arguments one by one. If the results match for all test cases, that is, for each input argument, the grader function returns logical true.
-Otherwise, it returns false. Note that in order to ensure that the comparison works for arrays and not just scalars, you should use the isequal function as opposed to the == 
-operator. Here are a few sample runs using built-in functions:
+%Here is you chance to write an autograder! Specifically, write a function called grader that tests two functions (one is supposed to be the student's solution and the other the
+%reference solution provided by the instructor) by calling them repeatedly with various input arguments and comparing the results. For simplicity, we assume that both functions take
+%exactly one input argument. The inputs to the grader function handles followed by a variable number of additional input arguments. The function must call the two
+%functions with each of the supplied input arguments one by one. If the results match for all test cases, that is, for each input argument, the grader function returns logical true.
+%Otherwise, it returns false. Note that in order to ensure that the comparison works for arrays and not just scalars, you should use the isequal function as opposed to the == 
+%operator. Here are a few sample runs using built-in functions:
 
 >>>grader(@sin,@max,0)
 ans =
